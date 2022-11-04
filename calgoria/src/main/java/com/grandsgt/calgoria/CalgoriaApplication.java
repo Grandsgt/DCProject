b9379f6fd1951ed2d7bf9f8dedd3156d36fa9f49
Grandsgt <watm@directlinegroup.co.uk>
2022-11-04 11:05:20 +0000

    First Azure deployment

---
+++ calgoria/src/main/java/com/grandsgt/calgoria/CalgoriaApplication.java
@@ -0,0 +1,13 @@
+package com.grandsgt.calgoria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalgoriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalgoriaApplication.class, args);
+	}
+
+}
---
+++ calgoria/src/test/java/com/grandsgt/calgoria/CalgoriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grandsgt.calgoria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalgoriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
